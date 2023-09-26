0f8d637504d9dce9eafc7f2cbc1ba1432720aeb8
Mustafa Mert Atıcı <mertatici97@gmail.com>
2023-09-26 21:58:18 +0300

    Add files via upload

--- girdiSayiyakadarciftsayilaribulan.java
+++ girdiSayiyakadarciftsayilaribulan.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class girdiSayiyakadarciftsayilaribulan {
     public static void main(String[] args) {
         Scanner inp=new Scanner(System.in);
-        int sayi;
+        int sayi,total = 0;
         System.out.println("Sayı giriniz");
         sayi= inp.nextInt();
 
         for (int i = 0; i <sayi ; i++) {
-            if (i%2==0) {
-                System.out.print(i+" ");
-            }
-        }
+            if (i%2==1) {
 
+                total=total+i;
+            }
+
+        }
+        System.out.println("total = " + total);
     }
 }
