210fa4a10d1f681ee148d5a051d8bc15ad0fe0a1
Mustafa Mert Atıcı <mertatici97@gmail.com>
2023-09-26 22:02:28 +0300

    Add files via upload

---
+++ girdiSayiyaKadarTekSayilariTopBulan.java
@@ -0,0 +1,21 @@
+package patika;
+
+import java.util.Scanner;
+
+public class girdiSayiyaKadarTekSayilariTopBulan {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        int sayi,total = 0;
+        System.out.println("Sayı giriniz");
+        sayi= inp.nextInt();
+
+        for (int i = 0; i <sayi ; i++) {
+            if (i%2==1) {
+
+                total=total+i;
+            }
+
+        }
+        System.out.println("total = " + total);
+    }
+}
