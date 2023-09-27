126588a20b3000d322464d8dbf4d0aedf067180b
Mustafa Mert Atıcı <mertatici97@gmail.com>
2023-09-27 20:46:18 +0300

    Add files via upload

---
+++ YildizlarElmas.java
@@ -0,0 +1,32 @@
+package patika;
+
+import java.util.Scanner;
+
+public class YildizlarElmas {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Basamk Sayı giriniz");
+        int n= inp.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=(n-i) ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=(2*i)-1 ; j++ ) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i>0 ; i--) {
+            for (int j = 1; j <=(n-i) ; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <=(2*i)-1 ; j++ ) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
