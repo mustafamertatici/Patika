8eafce6b196a4178b8c42e93dd3e464642b65887
Mustafa Mert Atıcı <mertatici97@gmail.com>
2023-09-29 19:00:09 +0300

    Add files via upload

---
+++ RecursivePower.java
@@ -0,0 +1,21 @@
+package patika;
+import java.util.Scanner;
+
+import static patika.MetotHesMak.power;
+
+public class RecursivePower {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input the number : ");
+        int number = input.nextInt();
+        System.out.print("Input the power : ");
+        int power = input.nextInt();
+        System.out.print(power(number, power));
+    }
+    public static int power(int number, int power){
+        if (power == 0) {
+            return 1;
+        }
+        return number*power(number,power-1);
+    }
+}
