c86cbdde90b0490202ff78e54a22221720435568
Mustafa Mert Atıcı <mertatici97@gmail.com>
2023-10-02 01:33:57 +0300

    Add files via upload

---
+++ MultidimensionalArrayToLetter.java
@@ -0,0 +1,27 @@
+package patika;
+
+public class MultidimensionalArrayToLetter {
+    public static void main(String[] args) {
+        String[][]letter=new String[7][4];
+
+        for (int i=0; i<letter.length; i++){
+            for (int j=0; j<letter[i].length; j++){
+                if (i==0 || i==2||i==4||i==6) {
+                    letter[i][j]=" * ";
+                } else if (j==0||j==3) {
+                    letter[i][j]=" * ";
+                }
+                else {
+                    letter[i][j]="   ";
+                    letter[3][3]="   ";
+                }
+            }
+        }
+        for (String[]row:letter){
+            for (String col: row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
